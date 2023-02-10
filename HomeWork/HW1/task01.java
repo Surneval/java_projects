9ae051122c5551445e9fdfc8c36d30509da35627
Surneval <nadia.trusova@yahoo.com>
2023-02-10 15:59:49 +0300

    HW1, Task01 is done

--- HomeWork/HW1/task01.java
+++ HomeWork/HW1/task01.java
@@ -1,12 +1,11 @@
 package HomeWork.HW1;
-import java.io.ObjectInputStream.GetField;
-import java.util.Arrays;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class task01 {
     // написать Написать программу вычисления n-ого треугольного числа
-    public static int GetNumber() {
+    public static int GetNumberFromUser() {
         Scanner iScanner = new Scanner(System.in);
         System.out.println("Enter a number > ");
         int number = Integer.parseInt(iScanner.nextLine());
@@ -14,10 +13,30 @@ public class task01 {
         return number;
     }
 
-    
+    public static int RandNumb(int minVal, int maxVal) {
+        Random random = new Random();
+        int number = random.nextInt(maxVal - minVal) + minVal;
+        return number;
+    }
+
+    public static int Triangle (int number){
+        int triangle = 0;
+        for(int i =0; i < number + 1; i ++){
+            triangle += i;
+        }
+        return triangle;
+    }
     public static void main(String[] args) {
         
-        //int number = GetNumber();
+        int number = GetNumberFromUser();
+        int triangle = Triangle(number);
+        System.out.printf("Triangle number of n = %d equals %d", number, triangle);
+        System.out.println('\n');
+
+        // random number
+
+        int rand = RandNumb(0,15);
+        System.out.printf("Random number  = %d\nTriangle number of %d equals %d", rand, rand, triangle);
 
     }
 }
