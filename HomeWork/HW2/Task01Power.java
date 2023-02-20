71b7b9264e3f6f2d0118cb874149a152eca65a8e
Surneval <nadia.trusova@yahoo.com>
2023-02-20 19:03:40 +0300

    HW 2 is done

--- HomeWork/HW2/Task01Power.java
+++ HomeWork/HW2/Task01Power.java
@@ -1,34 +1,36 @@
 package HomeWork.HW2;
+
 import java.io.*;
-import java.util.Scanner;
+import java.io.PrintWriter;
 
 public class Task01Power {
-    //Реализовать функцию возведения числа а в степень b. a, b ∈ Z.
-    //Сводя количество выполняемых действий к минимуму
-    public static String [] getDataFromText() {
-        String inputName = "input.txt";
-        Scanner sc = new Scanner(new File(inputName));
-        String [] array;
-
-        while(sc.hasNext()){
-            array = sc.nextLine().split(" ");
-        }
-        return array;
-    }
-
-    public static int expo(int a, int b){
-        int result = a;
-        while (b > 0){
-            result *= a;
-            b--;
-
-        }
-        return result;
-    }
-
+    // Реализовать функцию возведения числа а в степень b. a, b ∈ Z.
+    // Сводя количество выполняемых действий к минимуму
     public static void main(String[] args) {
-        String [] arrayA = getDataFromText();
-        System.out.println(arrayA);
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new FileReader("C:/Users/nadez/Desktop/java/HomeWork/HW2/input.txt"));
+            String aLine = reader.readLine().trim();
+            String bLine = reader.readLine().trim();
+            String[] arrayA = aLine.split("\\s+");
+            String[] arrayB = bLine.split("\\s+");
+            int a = Integer.parseInt(arrayA[1]);
+            int b = Integer.parseInt(arrayB[1]);
+            reader.close();
+            double result = a;
+            while (b > 0) {
+                result *= a;
+                b--;
+            }
+            PrintWriter writer = new PrintWriter(new File("output.txt"));
+            writer.printf("%s ^ %s = %.0f\n", arrayA[0], arrayB[0], result);
+            System.out.println(arrayA[0] + " raised to the power of " + arrayB[0] + " is " + result);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error reading input file: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input format: " + e.getMessage());
+        }
 
     }
 
